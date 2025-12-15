0c98604a5c4960cd65f1b780b7f2888e13815d14
Mohamed gomaa <99754570+mo7amedgom3a@users.noreply.github.com>
2025-12-15 18:28:34 +0200

    Remove '+' from greeting message

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello from Go + Kubernetes 🚀")
+	fmt.Fprintf(w, "Hello from Go Kubernetes 🚀")
 }
 
 func main() {
